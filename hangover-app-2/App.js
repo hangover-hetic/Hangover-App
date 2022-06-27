86fde0b0e4b8c2615f2387973fcce8dd15ece8d3
unknown <ernatusjessy@gmail.com>
2022-06-27 22:39:44 +0200

    add router

--- hangover-app-2/App.js
+++ hangover-app-2/App.js
@@ -1,24 +1,33 @@
-
-import React from 'react';
+import React            from 'react';
+import { View }         from 'react-native';
+import Homepage         from './pages/Homepage';
+import Login            from './pages/Login';
+import Register         from './pages/Register';
+import NotFound         from './pages/NotFound';
 import { 
-  StyleSheet, 
-  View 
-}            from 'react-native';
-import Index from './pages/Index';
+    NativeRouter, 
+    Route, 
+    Link 
+}                       from 'react-router-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-        <Index />
-    </View>
-  );
+    return <>
+        <NativeRouter>
+            <View>
+                <Route exact path="/" component={ Homepage } />
+                <Route path="/login" component={Login} />
+                <Route path="/register" component={Register} />
+                <Route element={<NotFound />} />
+            </View>
+        </NativeRouter>
+    </>;
 }
 
-const styles = StyleSheet.create({
+{/*const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+});*/}
