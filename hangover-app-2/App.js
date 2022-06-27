cf9e4f352f3345803dc00c2410ec89b7cc57f876
unknown <ernatusjessy@gmail.com>
2022-06-27 17:47:27 +0200

    setup redux in app and index

--- hangover-app-2/App.js
+++ hangover-app-2/App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  View 
+}            from 'react-native';
+import Index from './pages/Index';
 
 export default function App() {
   return (
     <View style={styles.container}>
-        <Text>Bienvenue sur hangover</Text>
-        <StatusBar style="auto" />
+        <Index />
     </View>
   );
 }
