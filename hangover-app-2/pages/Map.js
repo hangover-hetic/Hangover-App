1bd107494861f4b94ce58408469e1f9106114256
Tristan Seclet <tristanseclet@gmail.com>
2022-06-28 12:16:04 +0200

    begin map

---
+++ hangover-app-2/pages/Map.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Text from 'react-native';
+
+class Map extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Text>MAP</Text>
+        );
+    }
+}
