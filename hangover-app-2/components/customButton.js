6d24928e5b38b2bac5776f5744236252fe217505
unknown <ernatusjessy@gmail.com>
2022-06-29 21:12:31 +0200

    fix

--- hangover-app-2/components/customButton.js
+++ hangover-app-2/components/customButton.js
@@ -18,26 +18,26 @@ const SubmitButton = ({ title, onPress }) => {
 
 const styles = StyleSheet.create({
     container: {
-      shadowColor: '#000',
-      shadowOffset: {
+        shadowColor: '#000',
+        shadowOffset: {
         width: 0,
         height: 2,
-      },
-      backgroundColor: '#3F5EFB',
-      shadowOpacity: 0.23,
-      shadowRadius: 2.62,
-      width: 250,
-      elevation: 4,
-      borderRadius: 8,
-      height: 50,
-      justifyContent: 'center',
-      alignItems: 'center',
-      paddingHorizontal: 80,
+        },
+        backgroundColor: '#3F5EFB',
+        shadowOpacity: 0.23,
+        shadowRadius: 2.62,
+        width: 250,
+        elevation: 4,
+        borderRadius: 8,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingHorizontal: 80,
     },
     text: {
-      color: 'white',
-      fontSize: 16,
-      fontWeight: 'bold',
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
     },
   });
 
