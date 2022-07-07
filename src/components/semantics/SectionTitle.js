16673a00626684ea6bb74c01ab061952ea13c9bd
Tristan Seclet <tristanseclet@gmail.com>
2022-07-07 13:27:12 +0200

    work

--- src/components/semantics/SectionTitle.js
+++ src/components/semantics/SectionTitle.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Text } from 'react-native';
+import {StyleSheet} from 'react-native';
+import {Text} from 'react-native';
 
-class Container extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+class SectionTitle extends React.Component {
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    return (
-      <>
-        <Text style={styles.text}>{this.props.content}</Text>
-      </>
-    );
-  }
+    render() {
+        return <>
+            <Text style={styles.text}>
+                {this.props.content}
+            </Text>
+        </>;
+    }
 }
 
 const styles = StyleSheet.create({
-  text: {
-    color: '#ffffff',
-    fontSize: 20,
-    fontFamily: 'Poppins-SemiBold',
-  },
+    text: {
+        color: '#ffffff',
+        fontSize: 20,
+        fontFamily: 'Poppins-SemiBold',
+        marginBottom: 10
+    },
 });
 
-export default Container;
+export default SectionTitle;
