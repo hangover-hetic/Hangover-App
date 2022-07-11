fd4367480bc3b0d4deeaaf2ee9398a7278c888d5
timothee-durand <timothee.durand70@gmail.com>
2022-07-11 16:04:42 +0200

    add redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,19 @@
 import rootReducer from './rootReducer';
 import thunk from 'redux-thunk';
 import { legacy_createStore as createStore, applyMiddleware, compose } from 'redux';
+import ExpoFileSystemStorage from 'redux-persist-expo-filesystem';
+import { persistReducer } from 'redux-persist';
+
+const persistConfig = {
+  key: 'root',
+  storage: ExpoFileSystemStorage,
+};
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const reduxStore = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)));
+const reduxStore = createStore(
+  persistReducer(persistConfig, rootReducer),
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export default reduxStore;
