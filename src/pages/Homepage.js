b14c7f7c2f59bac5946d5132e18e9ef280a7cbb3
Tristan Seclet <tristanseclet@gmail.com>
2022-07-11 17:35:54 +0200

    closed carousel

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import { Text, View, ScrollView, Row, StyleSheet } from 'react-native';
+import {  View, ScrollView, } from 'react-native';
 import { connect } from 'react-redux';
 import RadioButton from '../components/RadioButton';
 import CustomButton from '../components/CustomButton';
 import SectionTitle from '../components/semantics/SectionTitle';
 import Container from '../components/ui/Container';
 import Span from '../components/semantics/Span';
+import CarouselContainer from "../components/ui/CarouselContainer";
+import {client} from "../api/client";
+import CardCarouselFestival from "../components/CardCarouselFestival";
 
 class Homepage extends React.Component {
   constructor(props) {
@@ -15,20 +18,30 @@ class Homepage extends React.Component {
       { value: 'Tous les évènements' },
       { value: 'Prochainement' },
     ];
+    this.state = {
+      festivals: [],
+    };
   }
 
   componentDidMount() {
     console.log(this.props.userToken);
+    client.get('festivals').then((response) => {
+      this.setState({festivals: response.data});
+    });
   }
+
 
   render() {
     return (
       <Container>
         <ScrollView>
           <RadioButton data={this.data} />
-
           <View>
             <SectionTitle content="Événements" />
+            <CarouselContainer
+                items={this.state.festivals}
+                renderItem={CardCarouselFestival}
+            />
           </View>
 
           <View>
